03a05f2e8b9d51436fcee68689608580bf4244c0
Roman <romanyakovlevwork@gmail.com>
2021-07-04 23:21:23 +0300

    Завершенные проекты

---
+++ my-app/src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app-header.css';
+
+const  Appheader = ({liked, allPosts}) => {
+    return (
+        <div className="app-header d-flex"> 
+            <h1>Roman Yakovlev</h1>
+            <h2>{allPosts} записей, из них понравилось {liked}</h2>
+        </div>
+    )
+    
+}
+
+export default Appheader;
